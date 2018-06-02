13cd1f71b397b7be22c3e853377c1a38dfd7beda
aliize <ap9387@student.uni-lj.si>
2018-06-02 17:21:29 +0200

    dodan vnos opisa slike

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -34,7 +34,7 @@ function($stateProvider, $urlRouterProvider, $windowProvider) {
                     deferred.reject();
                     return deferred.promise.catch(function () { $state.go('home'); });
                 }
-            }//*/
+            }
       }      
     })
     .state('prikazslik', {
@@ -47,8 +47,9 @@ function($stateProvider, $urlRouterProvider, $windowProvider) {
 }]);
 
 //controller za modalno okno
-app.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, povecana_slika) {
+app.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, povecana_slika, opis_slika) {
   $scope.povecana_slika = povecana_slika;
+  $scope.opis_povecana_slika = opis_slika;
 });
 
 app.controller('MainCtrl', [
@@ -57,32 +58,15 @@ function($scope, $http, $window, $location){
 
   $scope.galerije = [];
   $http.get('/vse_galerije').then(function(response) {
-    console.log("nalagam galerije....");
     $scope.galerije = response.data;
   });
 
   $scope.login_funkcija = function() {
-    //alert($scope.elektronska_posta + $scope.geslo);
     if (!$scope.elektronska_posta || !$scope.geslo) {
       $scope.pokazi_napako_login = true;
       $scope.login_status = "prosim vnesi ime in geslo za logiranje v sistem";
       return;
-    }    
-    /*$http.post('/prijava/preveriPrijavo',
-     {elektronska_posta: $scope.elektronska_posta, geslo: $scope.geslo}).then(function(response) {      
-      if (response.data.status == "200") {
-        $window.localStorage['galerija'] = response.data.token;
-        $scope.vpisanStudent = $window.localStorage['galerija'];
-        $scope.trenutni_uporabnik = ($scope.usernameTrenutnoVpisanega()).username;
-        $scope.id_trenutnega_uporabnika = ($scope.usernameTrenutnoVpisanega())._id;
-        $window.location.href = '/#!/galerije';
-      }
-      else {
-        //v primeru napake dobimo odgovor a je uporabniško ime al geslo narobe
-        $scope.login_status = response.data.vzrok;
-        $scope.pokazi_napako_login = true;
-      }
-    }); //*/
+    }
     else if ($scope.elektronska_posta == "ana" && $scope.geslo == "ana") {
       $window.localStorage['galerija'] = true;
       $window.location.href = '/#!/galerije';
@@ -137,31 +121,31 @@ function($scope, $http, $window, $location, $stateParams, $uibModal){
   $scope.ime_galerije = $stateParams.ime;
 
   $scope.slike_galerije = [];
+  $scope.opisi_slik = [];
   $http.get('/vse_slike_iz_galerije/'+$stateParams.ime).then(function(response) {    
-    $scope.slike_galerije = response.data;
+    $scope.slike_galerije = response.data.vseSlike;
+    $scope.opisi_slik = response.data.vsiOpisi;
   });
 
   if ($window.localStorage['galerija'])
     $scope.logged_in = true;
 
-  $scope.odpri_vecjo_sliko = function(slika) {
+  $scope.odpri_vecjo_sliko = function(slika,$index) {
     var modalInstance = $uibModal.open({
       controller: "ModalInstanceCtrl",
       templateUrl: '/myModalContent.html',
       windowClass: 'app-modal-window',
-      resolve: { povecana_slika: 
-          function() {
-            console.log(slika);
-            return slika;
-          }
+      resolve: { 
+        povecana_slika: function() {return slika;},
+        opis_slika: function() {return $scope.opisi_slik[$index];}
       }
     }).result.then(function(){}, function(res){});
   }
 
   $scope.zbrisi_sliko = function(galerija_ime, slika_ime, $index) {
-    console.log(slika_ime);
-    $http.get('/zbrisi_sliko/'+galerija_ime+'/'+slika_ime).then(function(response) {      
+    $http.get('/zbrisi_sliko/'+galerija_ime+'/'+$scope.slike_galerije[$index]+'/'+$scope.opisi_slik[$index]).then(function(response) {      
       $scope.slike_galerije.splice($index, 1);
+      $scope.opisi_slik.splice($index, 1);
     });
   }
 
--- routes/index.js
+++ routes/index.js
@@ -10,35 +10,21 @@ path = require('path');
 var FileSaver = require('file-saver');
 
 
-var uploading = multer({
-  dest: './public/'
-})
-
-var schema = new mongoose.Schema({ src:'string', type:'string'});
-var Slika = mongoose.model('Slika', schema);
-
-//galerija hrani json-e slik
-var schema_galerija = new mongoose.Schema({ ime:'string', vseSlike: ['string']});
+var uploading = multer({dest: './public/'});
+var schema_galerija = new mongoose.Schema({ ime:'string', vseSlike: ['string'], vsiOpisi: ['string']});
 var Galerija = mongoose.model('Galerija', schema_galerija);
 
 
-
-
-//dobi vse galerije
 router.get('/vse_galerije', function(req, res) {
 	Galerija.find(function(err, galerije) {
 		if (err) throw err;
 		res.send(galerije);
 	});
 });
-//ustvari novo galerijo
-//preverjaj da ni istega imena že v bazi
+
 router.get('/nova_galerija/:ime', function(req, res) {
-	//preverimo da galerija s tem imenom še ne obstaja
 	Galerija.findOne({'ime':req.params.ime}, function(err, ze_obstojeca) {
 		if (err) throw err;
-		console.log(ze_obstojeca);
-		//če že obstaja z istim imenom, ne ustvarimo še ene
 		if (ze_obstojeca != null) {
 			res.send("Galerija s tem imenom že obstaja");
 		}
@@ -57,7 +43,6 @@ router.get('/nova_galerija/:ime', function(req, res) {
 	
 });
 
-//zbriše galerijo
 router.get('/zbrisi_galerija/:ime', function(req, res) {
 	Galerija.remove({'ime':req.params.ime}, function(err) {
 		if (err) throw err;
@@ -65,8 +50,6 @@ router.get('/zbrisi_galerija/:ime', function(req, res) {
 	});
 });
 
-
-//dobi vse slike iz določene galerije
 router.get('/vse_slike_iz_galerije/:galerija', function(req, res) {
 	Galerija.findOne({'ime':req.params.galerija}, function(err, galerija) {
 		if (err) throw err;
@@ -74,53 +57,25 @@ router.get('/vse_slike_iz_galerije/:galerija', function(req, res) {
 			console.log("ta galerija ne obstaja");
 			return [];
 		}
-		res.send(galerija.vseSlike);
+		res.send(galerija);
 	});
 });
 
-//shrani sliko v določeno galerijo
-//preveri da galerija sploh obstaja
-router.post('/shrani_sliko_v_galerijo/:galerija', uploading.single('upl'), function(req, res) {
-	Galerija.findOneAndUpdate({ime:req.params.galerija}, {$push:{vseSlike: req.file.filename}}, 
+router.post('/shrani_sliko_v_galerijo/:galerija/:opis', uploading.single('upl'), function(req, res) {
+	Galerija.findOneAndUpdate({ime:req.params.galerija}, {$push:{vseSlike: req.file.filename, vsiOpisi: req.params.opis}}, 
 		function(err, galerija) {
 		if (err) res.send("napaka");		
 		res.redirect('/#!/galerija/'+req.params.galerija);
 	});
 });
 
-
-//zbriše sliko iz galerije......
-router.get('/zbrisi_sliko/:galerija_ime/:slika_ime', function(req, res) {
-	Galerija.findOneAndUpdate({ime:req.params.galerija_ime}, {$pull:{vseSlike: req.params.slika_ime}}, 
+router.get('/zbrisi_sliko/:galerija_ime/:slika_ime/:opis', function(req, res) {
+	Galerija.findOneAndUpdate({ime:req.params.galerija_ime}, {$pull:{vseSlike:req.params.slika_ime, vsiOpisi:req.params.opis}}, 
 		function(err, galerija) {
 		if (err) res.send("napaka");		
 		res.redirect('/#!/galerija/'+req.params.galerija_ime);
 	});
 });
-
-
-/*router.get('/register/:username/:password', function(req, res) {
-	var user = new User({username:req.params.username});
-	user.setPassword(req.params.password);
-
-	user.save(function(err){
-		if (err) throw err;
-		res.send({token: user.generateJWT()})
-	});
-});//*/
-
-/*router.post('/prijava/preveriPrijavo', function(req, res, next){
-	req.body.username = req.body.elektronska_posta;
-	req.body.password = req.body.geslo;
-
-  passport.authenticate('local', function(err, user, info){
-    if(err) throw err;
-
-    if(user) res.send({status: "200", token: user.generateJWT()});
-	else res.send({status:"401", vzrok: info});
-  })(req, res, next);
-});//*/
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
